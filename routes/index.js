a7689f08296ad6a125fd2d5bb5bded9a66152f10
stevenn2012 <2012stevenn@gmail.com>
2017-05-20 17:33:35 -0500

    ckeckout como store, arreglar grilla mk

--- routes/index.js
+++ routes/index.js
@@ -44,7 +44,7 @@ router.get('/login',function(req,res){
 });
 
 router.get('/checkout',function(req,res){
-	res.render('checkout');
+	res.render('checkout', { title: 'Tienda sin autenticar', User: undefined });
 });
 
 function ensureAuthenticated(req, res, next){
