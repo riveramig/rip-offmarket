7b043a73d084b904a65c060553c1eac25f3f297e
santiagojersson <santiagojersson@gmail.com>
2017-05-20 14:04:37 -0500

    Corregidass las rutas del usuario con el nuevo template

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,39 @@ var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/',function(req, res, next) {
-  res.render('index', { title: 'Tienda sin autenticar', User:{name: ""}});
+  res.render('index', { title: 'Tienda sin autenticar', User: undefined });
 });
 
 router.get('/users', ensureAuthenticated,function(req, res, next) {
-  res.render('index', { title: 'Tienda Autenticada el Usuarios es:', User: req.user });
+	var usuario=undefined;
+	if (req.user.local.name != undefined) {
+		usuario=req.user.local.name;
+	} else if (req.user.facebook.name != undefined) {
+		usuario=req.user.facebook.name;
+	} else if (req.user.twitter.displayName != undefined) {
+		usuario=req.user.twitter.displayName;
+	} else {
+		usuario=req.user.google.name;
+	}
+	console.log(usuario);
+	
+  res.render('index', { title: 'Tienda Autenticada el Usuarios es:', User: usuario });
 });
 
-router.get('/cart',function(req,res){
-  res.render('cart');
+router.get('/cart', ensureAuthenticated,function(req, res, next){
+  var usuario=undefined;
+	if (req.user.local.name != undefined) {
+		usuario=req.user.local.name;
+	} else if (req.user.facebook.name != undefined) {
+		usuario=req.user.facebook.name;
+	} else if (req.user.twitter.displayName != undefined) {
+		usuario=req.user.twitter.displayName;
+	} else {
+		usuario=req.user.google.name;
+	}
+	console.log(usuario);
+	
+  res.render('cart', { title: 'Tienda Autenticada el Usuarios es:', User: usuario });
 });
 
 router.get('/login',function(req,res){
