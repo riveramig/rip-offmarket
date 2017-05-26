2475fad4feced0d269740bce92a7e64ce3732bf4
SanMalparido <rivera-miguel@hotmail.com>
2017-05-26 17:15:39 -0500

    Carro de compras homosexual creado, falta compra en la base de datos

--- public/index/indexController.js
+++ public/index/indexController.js
@@ -1,16 +1,20 @@
-angular.module('Index',['ngCookies'])
-	.controller('mainCtrl',['productService','categoryService','cartService',function(productService,categoryService,cartService){
+angular.module('Index',['Factory'])
+	.controller('mainCtrl',['$window','productService','categoryService','cartService',function($window,productService,categoryService,cartService){
 		console.log('index controller created');
 		var self=this;
-		self.index=4;
+		self.indexCount=0;
 		self.productos=productService.full();
 		self.categories=categoryService.full();
-		self.addCart=function(p){
-			cartService.add(p);
-		};
+		self.addCart=function(product){
+			cartService.add(product);
+		}
 		self.cart=cartService.merca();
-		self.getTotal=function(){
-			return cartService.getTotal();
+		self.logOut=function(){
+			cartService.clear();
+		}
+		self.remove=function(p){
+			cartService.remove(p);
+			$window.location.reload();
 		}
 	}])
 	.factory('productService',['$http',function($http){
@@ -38,33 +42,6 @@ angular.module('Index',['ngCookies'])
 		return{
 			full:function(){
 				return data;
-			}
-		};
-	}])
-	.factory('cartService',['$cookies',function($cookies){
-		var cart=[];
-		$cookies.put('MyCarroHomosexual',cart);
-		return{
-			add: function(data){
-				console.log(data);
-				console.log(cart);
-				cart.push(data);
-				$cookies.put('MyCarroHomosexual',cart);
-			},
-			merca: function(){
-				var cookieGay=$cookies.get('MyCarroHomosexual');
-				return cookieGay;
-			},
-			remove: function(p){
-				var index=cart.indexOf(p);
-				cart.splice(index,1);
-			},
-			getTotal: function(){
-				var total=0;
-				for(var a in cart){
-					total=total+parseInt(cart[a].price);
-				}
-				return total;
 			}
 		};
 	}])
